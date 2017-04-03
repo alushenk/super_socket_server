7e70cdbecc012743f23a32ef9f00ef6cb5fa8bd1
Anton Anatolievich <wartoxy@gmail.com>
2017-04-03 21:50:19 +0300

    implementing errorpage, handling exceptions

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3.5
-# coding=utf-8
 
 import socket
 import os
 import time
 import sys
 import mimetypes
+from http import HTTPStatus
+
+def error_page(err_code='404', mess=' ', err_code_expln=' '):
+    err_page = "<!DOCTYPE html><html><head>"
+    err_page += "<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">"
+    err_page += "<title>Error response</title></head><body>"
+    err_page += "<h1>Error response</h1>"
+    err_page += "<p>Error code: {:s}</p>".format(err_code)
+    err_page += "<p>Message: {:s}.</p>".format(mess)
+    err_page += "<p>Error code explanation: {:s}.</p>".format(err_code_expln)
+    err_page += "</body></html>"
+    return err_page
 
 def send_answer(conn, status="200 OK", typ="text/plain; charset=utf-8", data=""):
-	#data = data.encode("utf-8")
+	if "text" in typ:
+		data = data.encode("utf-8")
 	conn.send(b"HTTP/1.1 " + status.encode("utf-8") + b"\r\n")
 	conn.send(b"Server: simplehttp\r\n")
 	conn.send(b"Connection: close\r\n")
@@ -19,17 +31,19 @@ def send_answer(conn, status="200 OK", typ="text/plain; charset=utf-8", data="")
 
 def file_manager(address):
 
-	# проверить, считали ли мы хоть шото и валидный ли данный путь. трай-екзепт как в примере
-	content = os.listdir(path='.' + address)
-	content.sort(key = lambda a: a.lower())
+	try:
+		content = os.listdir(path='.' + address)
+	except OSError:
+		return error_page(HTTPStatus.NOT_FOUND, "No permission to list directory")
 
+	content.sort(key = lambda a: a.lower())
 	for index in "index.html", "index.htm":
 		if index in content:
 			with open('.' + address + index) as page:
 				return page.read()
 
 	result = "<!DOCTYPE html>"
-	result += "<html><head><title>Directory listing for</title></head><body>"
+	result += "<html><head><title>Directory listing for {0:s}</title></head><body>"
 	result += "<h1>Directory listing for {0:s}</h1><hr><ul>".format(address)
 	for elem in content:
 		if os.path.isdir('.' + address + elem):
@@ -57,15 +71,28 @@ def parse_data(client_sock, client_addr):
 		send_answer(client_sock, "404 Not Found", data = "404 Not Found")
 		return
 
+	#address = address.encode("utf-8")
+	#print(address)
 	if os.path.isdir('.' + address):
 		answer = file_manager(address)
-		typ = "text/html"
-	elif os.path.isfile('.' + address):
-		print('eta fail')
-		with open('.' + address, 'rb') as file:
-			answer = file.read()
+		typ = "text/html; charset=utf-8"
+	else: #os.path.isfile('.' + address):
 		typ = extensions_map[os.path.splitext(address)[1]]
-	typ += "; charset=utf-8"
+		if "text" in typ:
+			typ += "; charset=utf-8"
+			file = open('.' + address)
+		else:
+			file = open('.' + address, 'rb')
+		answer = file.read()
+		file.close()
+
+		# if "image" in typ:
+		# 	with open('.' + address, 'rb') as file:
+		# 		answer = file.read()
+		# else:
+		# 	with open('.' + address) as file:
+		# 		answer = file.read()
+		
 	print(typ)
 	send_answer(client_sock, typ=typ, data=answer)
 
@@ -84,12 +111,14 @@ def get_port(argv, result):
 
 mimetypes.init()
 extensions_map = mimetypes.types_map.copy()
-extensions_map[""] = "application/octet-stream"
-if '' in extensions_map:
-	print(extensions_map[''])
+extensions_map.update({
+	'': 'application/octet-stream',
+	'.py': 'text/plain',
+	'.c': 'text/plain',
+	'.h': 'text/plain'
+	})
 host_port = get_port(sys.argv, 8000)
 host_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-#hostname = socket.gethostname()
 host_sock.bind(('', host_port))
 print("Serving HTTP on %s port %d ..."% host_sock.getsockname())
 host_sock.listen(10)
